248b637276aa638be93b3459c46f0846002e360f
Geovani Cavalcante <geovani.scavalcante@gmail.com>
2024-03-10 21:34:26 -0300

    first commit

---
+++ src/main/java/com/geovanicv/vim/test/VimTestApplication.java
@@ -0,0 +1,13 @@
+package com.geovanicv.vim.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VimTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VimTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geovanicv/vim/test/VimTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geovanicv.vim.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VimTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
